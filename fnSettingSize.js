bbfa10dcd17a742a11b1d9bfdeb0dc7d04754d9f
villethan720 <93165624+villethan720@users.noreply.github.com>
2022-02-28 20:36:37 -0500

    Add files via upload

---
+++ fnSettingSize.js
@@ -0,0 +1,31 @@
+function enlargeText() {
+    document.getElementById('p1').style.fontsize = "7em"; 
+    document.getElementById('p2').style.fontsize = "7em";
+    document.getElementById('p3').style.fontsize = "7em"; 
+    document.getElementById('p4').style.fontsize = "7em"; 
+    document.getElementById('p5').style.fontsize = "7em";  //5px works as well
+}
+
+function decreaseText(){
+    document.getElementById('p1').style.fontsize = "1em";
+    document.getElementById('p2').style.fontsize = "1em";
+    document.getElementById('p3').style.fontsize = "1em";
+    document.getElementById('p4').style.fontsize = "1em";
+    document.getElementById('p5').style.fontsize = "1em";
+}
+
+function enlargeHeadingOne(){
+    document.getElementById('h1').style.fontsize = "9em"
+}
+
+function enlargeHeadingTwo(){
+    document.getElementById('h2').style.fontsize = "9em"
+}
+
+function decreaseHeadingOne(){
+    document.getElementById('h1').style.fontsize = "2em"
+}
+
+function decreaseHeadingTwo(){
+    document.getElementById('h2').style.fontsize = "2em"
+}
---
+++ hidingImages.js
@@ -0,0 +1,19 @@
+function hide(){
+    document.getElementById('winter').className = "hidden";
+}
+
+function doNotShow(){
+    document.getElementById('dog').className = "hidden";
+}
+
+function invisible(){
+    document.getElementById('temple').className = "hidden";
+}
+
+function disappear(){
+    document.getElementById('sand').className = "hidden";
+}
+
+function goAway(){
+    document.getElementById('bird').className = "hidden";
+}
